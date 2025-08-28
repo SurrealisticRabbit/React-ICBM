97e0338b4713ac7852c8f202a87b10869ffe776e
Ducky Rabbit <ducky@rabbit.net>
2025-08-28 20:26:23 +0100

    Update vite config for gh pages

--- icbm-web/vite.config.js
+++ icbm-web/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // This base path is the name of your repository on GitHub
+  base: '/React-ICBM/', 
   plugins: [react()],
 })
